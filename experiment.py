c4c613cf87ca69fc37e9c1a85877751f74d6844d
BebDong_win <bebdong@outlook.com>
2018-10-23 17:52:06 +0800

    experiment complete with MNIST dataset

---
+++ experiment.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+# author: BebDong
+# 2018.10.23
+
+import neural_network as nn
+import numpy
+
+# 指定神经网络的结构。隐藏层节点个数不唯一
+input_nodes, hidden_nodes, output_nodes = 784, 100, 10
+
+# 指定权重更新的学习率
+learning_rate = 0.3
+
+# 创建神经网络的实例
+network = nn.NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+# 读取训练数据，只读方式
+training_data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
+# 当数据集很大时，应当分批读入内存。这里仅100条记录，则一次性全部读入内存
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+# 训练神经网络
+for record in training_data_list:
+    # 缩放输入
+    all_pixels = record.split(',')
+    scaled_inputs = (numpy.asfarray(all_pixels[1:])/255.0 * 0.99) + 0.01
+    # 创建目标输出
+    targets = numpy.zeros(output_nodes) + 0.01
+    targets[int(all_pixels[0])] = 0.99
+    network.training(scaled_inputs, targets)
+    pass
+
+# 读取测试数据集
+test_data_file = open("mnist_dataset/mnist_test_10.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+# 测试训练好的神经网络
+# 初始化一个数据结构用于记录神经网络的表现
+scorecard = []
+# 遍历测试数据集
+for record in test_data_list:
+    # 打印预期输出
+    all_pixels = record.split(',')
+    correct_label = int(all_pixels[0])
+    print("correct label: ", correct_label)
+    # 查询神经网络
+    inputs = (numpy.asfarray(all_pixels[1:])/255.0 * 0.99) + 0.01
+    outputs = network.query(inputs)
+    answer = numpy.argmax(outputs)
+    print("network's answer: ", answer)
+    # 更新神经网络的表现
+    if answer == correct_label:
+        scorecard.append(1)
+    else:
+        scorecard.append(0)
+        pass
+    pass
+
+# 打印得分
+print(scorecard)
+print("performance: ", sum(scorecard) / len(scorecard))
--- test.py
+++ test.py
@@ -5,6 +5,9 @@
 import numpy
 import matplotlib.pyplot as plt
 
+# 直接使用plt.imshow无法显示图片，需要导入pylab包
+import pylab
+
 # 打开并读取文件
 data_file = open("mnist_dataset/mnist_train_100.csv")
 data_list = data_file.readlines()
@@ -15,3 +18,14 @@ all_pixels = data_list[0].split(',')
 image_array = numpy.asfarray(all_pixels[1:]).reshape((28, 28))
 plt.figure("Image")
 plt.imshow(image_array, cmap='gray', interpolation='None')
+pylab.show()
+
+# 缩放输入数据。0.01的偏移量避免0值输入
+scaled_inputs = (numpy.asfarray(all_pixels[1:])/255.0 * 0.99) + 0.01
+
+# 构建目标矩阵。sigmoid函数无法取端点值0或者1，使用0.01代替0，0.99代替1
+output_nodes = 10
+# 产生0值输出矩阵
+targets = numpy.zeros(output_nodes) + 0.01
+# 将字符串转换为整数，并设置激发节点
+targets[int(all_pixels[0])] = 0.99
